d8377bcd0656383c20c98d22ef66237a738bc6ff
David Radcliffe <dradcliffe@gmail.com>
2018-08-08 11:28:06 -0500

    Add test

--- lab-02-first-component/src/App.test.js
+++ lab-02-first-component/src/App.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import App from './App';
 
 describe('App Component', () => {
   it('renders without exploding', () => {
     expect(shallow(<App />)).toHaveLength(1);
+  });
+
+  it('contains the Hello component', () => {
+    const app = mount(<App />);
+    expect(app.find('Hello')).toExist();
   });
 });
